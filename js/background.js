0595f5c0a3094c95a8b1e463bda0935f16d385d8
rentamob <aghorler@gmail.com>
2015-11-30 22:44:00 +1100

    Added check for IPHandling undefined

--- js/background.js
+++ js/background.js
@@ -1,3 +1,19 @@
+/*	webRTCIPHandlingPolicy, if undefined, set to 'default_public_interface_only'.
+	This setting is only supported from Chromium version 48. */
+chrome.storage.local.get(null, function(items) {
+	if (items.rtcIPHandling == undefined) {
+		chrome.storage.local.set({
+			rtcIPHandling: 'default_public_interface_only'
+		}, function() {
+			chrome.privacy.network.webRTCIPHandlingPolicy.set({
+				value: 'default_public_interface_only'
+			});
+		})
+	}
+});
+
+/*	webRTCMultipleRoutesEnabled statically set to false.
+	This option is deprecated from Chromium version 48. */
 chrome.privacy.network.webRTCMultipleRoutesEnabled.set({
 	value: false,
 	scope: 'regular'
